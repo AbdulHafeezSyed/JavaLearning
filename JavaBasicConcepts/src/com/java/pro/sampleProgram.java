4897990b720d9e058f108c2db1d695f676f39120
AbdulHafeezSyed <hafeezsyed294@gmail.com>
2025-07-11 12:31:16 +0530

    sample program

---
+++ JavaBasicConcepts/src/com/java/pro/sampleProgram.java
@@ -0,0 +1,11 @@
+package com.java.pro;
+
+public class sampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcom to java");
+
+	}
+
+}
